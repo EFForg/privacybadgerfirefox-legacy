19b4c2ec4d999bc95d35ca5257aed3653e25b05b
Yan Zhu <yan@mit.edu>
2014-04-30 09:47:38 -0700

    Coding style fixes

--- test/test-heuristic-blocker.js
+++ test/test-heuristic-blocker.js
@@ -22,20 +22,20 @@ exports.testHasTracking = function(assert){
   let channelInfo = {
     origin: 'localhost',
     parentOrigin: 'example.com'
-  }
+  };
   let channel = {
     URI: newURI("http://localhost/")
   };
 
-  let cookies = {test:"foo", privacy:"badger"};
-  assert.ok(heuristicBlocker.hasTracking(channel,channelInfo,cookies), 
+  let cookies = { test:"foo", privacy:"badger" };
+  assert.ok(heuristicBlocker.hasTracking(channel,channelInfo,cookies),
             "blocks high entropy cookies");
 
-  let cookies = {test:"1", test2:"2", test3:"3"};
+  let cookies = { test:"1", test2:"2", test3:"3" };
   assert.ok(heuristicBlocker.hasTracking(channel,channelInfo,cookies),
            "blocks multiple low entropy cookies");
 
-  let cookies = {dnt:"1"};
+  let cookies = { dnt:"1" };
   assert.ok(!heuristicBlocker.hasTracking(channel,channelInfo,cookies),
            "does not block < gMaxCookieEntropy bits of low entropy cookies");
 };
