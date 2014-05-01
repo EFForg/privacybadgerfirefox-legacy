892279d1e9d760890cc0900011b75039b7e5745c
Yan Zhu <yan@mit.edu>
2014-05-01 14:21:55 -0700

    Rezip file after changing rdf

--- release-utils/make-signed-xpi.sh
+++ release-utils/make-signed-xpi.sh
@@ -19,14 +19,22 @@ fi
 # Final XPI name
 XPI_NAME="$APP_NAME-$1.xpi"
 
-[ -d pkg ] || mkdir pkg
+rm -rf pkg/
+mkdir pkg
 
 # Build the unsigned XPI and unzip it
+echo "Running cfx xpi"
 cfx xpi
-unzip -d pkg $PRE_XPI_NAME
-rm $PRE_XPI_NAME
+unzip -q -d pkg "$PRE_XPI_NAME"
+rm "$PRE_XPI_NAME"
 
 # Customize install.rdf with our updateKey and URL
 sed -i 's@</RDF>@\n@g' pkg/install.rdf
 cat install-template.rdf >> pkg/install.rdf
 
+# Rezip the XPI
+rm -f "$XPI_NAME"
+cd pkg
+zip -q -X -9r "$XPI_NAME" .
+
+echo "Created $XPI_NAME in $(pwd)"
