43454007da4cc281934ba652af65db34ae40f89d
Cooper Quintin <cooperq@eff.org>
2015-06-04 13:34:13 -0700

    options page can manipulate disabled sites

--- data/options.js
+++ data/options.js
@@ -9,7 +9,11 @@ function loadOptions() {
     //$('#blockedResourcesContainer').on('mouseenter', '.tooltip', displayTooltip);
     //$('#blockedResourcesContainer').on('mouseleave', '.tooltip', hideTooltip);   
     $('#blockedResourcesContainer').on('click', '.userset .honeybadgerPowered', revertDomainControl);
+
+    $('.addButton').click(addDomainException);
+    $('.removeButton').click(removeDomainExceptions);
   });
+
 
   // Display jQuery UI elements
   $("#tabs").tabs();
@@ -17,7 +21,47 @@ function loadOptions() {
   $(".refreshButton").button("option", "icons", {primary: "ui-icon-refresh"});
   $(".addButton").button("option", "icons", {primary: "ui-icon-plus"});
   $(".removeButton").button("option", "icons", {primary: "ui-icon-minus"});
+
+  // Request settings from addon - defined in lib/ui.js
+  self.port.emit('reqSettings');
+
+  self.port.on('recvSettings', function(storage){
+    console.log('got storage');
+    loadDisabledSites(storage.disabledSites);
+  });
 }
 $(loadOptions);
 
+function updateOrigin(){
+  return;
+}
 
+function revertDomainControl(){
+  return;
+}
+
+function loadDisabledSites(disabledSites){
+  $('#excludedDomainsBox').empty();
+  $.each(disabledSites, function(key) {   
+    $('#excludedDomainsBox')
+      .append($('<option>', { value : key })
+      .text(key)); 
+  });
+}
+
+function removeDomainExceptions(event){
+  event.preventDefault();
+  var selected = $(document.getElementById("excludedDomainsBox")).find('option:selected');
+  var domains = [];
+  for(let i = 0; i < selected.length; i++){
+    domains.push(selected[i].text);
+  }
+  self.port.emit('removeFromDisabledSites', domains);
+}
+
+function addDomainException(event){
+  event.preventDefault();
+  var domain = $('#newWhitelistDomain').val();
+  $('#newWhitelistDomain').val('');
+  self.port.emit('addToDisabledSites', domain);
+}
