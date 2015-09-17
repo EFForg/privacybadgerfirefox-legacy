a5d93044d5279cd17576526f09f01416f81840ca
Cooper Quintin <cooperq@eff.org>
2015-09-16 19:20:59 -0700

    add test for #516

--- test/test-content-policy.js
+++ test/test-content-policy.js
@@ -1,5 +1,8 @@
 const utils = require("../lib/utils.js");
 const { Policy } = require("../lib/contentPolicy.js");
+const userStorage = require("../lib/userStorage.js");
+userStorage.init();
+
 
 exports["test Policy hasWhitelistedScheme"] = function(assert) {
   let urls = {
@@ -16,5 +19,30 @@ exports["test Policy hasWhitelistedScheme"] = function(assert) {
                  url + " " + (expected ? "should be" : "should not be") + " whitelisted");
   }
 };
+
+let preloads = "\
+@@||google.com^$third-party\n\
+@@||eff.org^$third-party\n\
+@@||notrack.malicious.org^$third-party\n\
+"
+userStorage.syncPreloads(preloads);
+
+exports["test isPreloadedWhitelistRequest"] = function(assert){
+  let urls = {
+    "http://eff.org": true,
+    "http://www.eff.org": true,
+    "http://maps.google.com": true,
+    "http://malicious.org": false,
+    "http://notrack.org": false,
+    "http://super.long.sub.domain.at.eff.org": true
+  }
+
+  for (let url in urls) {
+    let expected = urls[url];
+    assert.equal(Policy._isPreloadedWhitelistRequest(utils.makeURI(url)),
+                 expected,
+                 url + " " + (expected ? "should be" : "should not be") + " whitelisted");
+  }
+}
 
 require("sdk/test").run(exports);
