2063fe349504c86fb69403e8d489fd505e011863
Rob Cutmore <robcutmore@gmail.com>
2016-03-11 07:46:42 -0500

    Avoid filtering domains while user is typing

--- data/options.js
+++ data/options.js
@@ -235,22 +235,34 @@ function showTrackingDomainStats(domains) {
  * @param event Input event triggered by user.
  */
 function filterTrackingDomains(event) {
-  let domains = [];
-  let searchText = $('#trackingDomainSearch').val().toLowerCase();
+  let initialSearchText = $('#trackingDomainSearch').val().toLowerCase();
 
-  for (let trackingDomain in originCache) {
-    // Ignore object properties.
-    if (! originCache.hasOwnProperty(trackingDomain)) {
-      continue;
+  // Wait a short period of time and see if search text has changed.
+  // If so it means user is still typing so hold off on filtering.
+  let timeToWait = 500;
+  setTimeout(function() {
+    // Check search text.
+    let searchText = $('#trackingDomainSearch').val().toLowerCase();
+    if (searchText !== initialSearchText) {
+      return;
     }
 
-    // Ignore domains that do not contain search text.
-    if (trackingDomain.toLowerCase().indexOf(searchText) !== -1) {
-      domains.push(trackingDomain);
-    }
-  }
+    // Filter tracking domains based on search text.
+    let domains = [];
+    for (let trackingDomain in originCache) {
+      // Ignore object properties.
+      if (! originCache.hasOwnProperty(trackingDomain)) {
+        continue;
+      }
 
-  showTrackingDomains(domains);
+      // Ignore domains that do not contain search text.
+      if (trackingDomain.toLowerCase().indexOf(searchText) !== -1) {
+        domains.push(trackingDomain);
+      }
+    }
+
+    showTrackingDomains(domains);
+  }, timeToWait);
 }
 
 /**
